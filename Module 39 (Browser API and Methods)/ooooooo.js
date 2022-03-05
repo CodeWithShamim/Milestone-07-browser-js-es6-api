7e19b0815fe5696f4c3b78196719449eb670ad8b
codewithshamim <shamimislamonline@gmail.com>
2022-03-05 16:49:47 +0600

    send data in local storage

--- Module 39 (Browser API and Methods)/ooooooo.js
+++ Module 39 (Browser API and Methods)/ooooooo.js
@@ -1,23 +1,26 @@
 const addItem = () => {
     const item = document.getElementById('item');
     const itemValue = item.value;
-    console.log(itemValue);
-    // add cart to local storage
-    addCartToLocal(itemValue)
-    item.value = '';
+
+    // add to local storage 
+    addProductToCart(itemValue)
+    item.value = "";
+
 }
 
-// display data from local storage 
+// display data in UI----------------------------------------------------------
 const display = () => {
     const ul = document.getElementById('ul');
     const li = document.createElement('li');
-    li.innerText = 12;
+    const cartValue = localStorage.getItem('cart');
+    const cartValueParse = JSON.parse(cartValue);
+    console.log(cartValueParse);
+    li.innerText = cartValueParse.iphone;
     ul.appendChild(li);
+}
 
-};
 
-
-// get value from local storage 
+// add to in local storage -----------------------------------------------
 const getCart = () => {
     const cart = localStorage.getItem('cart');
     let cartObj;
@@ -27,13 +30,12 @@ const getCart = () => {
         cartObj = {};
     }
     return cartObj;
-};
+}
 
-// add cart to local storage
-const addCartToLocal = (itemValue) => {
+const addProductToCart = (itemValue) => {
     const cart = getCart();
-    // set property and value in object 
     cart[itemValue] = 1;
+    // console.log(cart);
     const cartStringify = JSON.stringify(cart);
     localStorage.setItem('cart', cartStringify);
 };
