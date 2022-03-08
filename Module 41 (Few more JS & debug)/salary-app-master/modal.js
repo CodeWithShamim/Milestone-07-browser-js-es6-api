6ee65752bfc354e34bc9e928c6252c6f8acf6d17
codewithshamim <shamimislamonline@gmail.com>
2022-03-08 19:48:32 +0600

    add modal

--- Module 41 (Few more JS & debug)/salary-app-master/modal.js
+++ Module 41 (Few more JS & debug)/salary-app-master/modal.js
@@ -15,7 +15,7 @@ const getData = (data) => {
         div.innerHTML = `<div class='text-center my-4 bg-warning'>
        <h3 class='py-3'> Name: ${item.strMeal}<br/><h3/>
         <img src='${item.strMealThumb}'/ class='rounded img-fluid'><br/>
-        <button data-bs-toggle="modal" data-bs-target="#staticBackdrop" class="bg-primary my-3 text-white border-0 p-3 rounded fw-bold" onclick="getModal()">Details</button>
+        <button data-bs-toggle="modal" data-bs-target="#phone-details" class="bg-primary my-3 text-white border-0 p-3 rounded fw-bold" onclick="getModal(${item.idMeal})">Details</button>
         <div/>
         `;
 
@@ -23,45 +23,37 @@ const getData = (data) => {
     }
 };
 
-const getModal = () => {
+const getModal = (id) => {
 
-
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=a')
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
         .then(res => res.json())
+        // .then(data => console.log(data));
         .then(data => showData(data.meals[0]));
 
 
 };
 
 const showData = (data) => {
-    const allItem = document.getElementById('allItem');
+    const modalContent = document.getElementById('modal-content');
+    modalContent.setAttribute('id', 'modal-content');
+
     const div = document.createElement('div');
+    div.className = "";
     div.innerHTML = `
-    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <div class="w-50 mx-auto">
-                            <h5 class="modal-title" id="staticBackdropLabel">Details</h5>
-                        </div>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body ">
-                        <h6>${data.strMeal}<h6/>
-                        <img src="./images/favicon.png " alt=" ">
-                        <img src="./images/favicon.png " alt=" ">
-                        <img src="./images/favicon.png " alt=" ">
-                        <img src="./images/favicon.png " alt=" ">
-                    </div>
-                    <div class="modal-footer ">
-                        <button type="button " class="btn btn-secondary " data-bs-dismiss="modal ">Close</button>
-                        <button type="button " class="btn btn-primary ">ok</button>
-                    </div>
-                </div>
+            <div class="modal-header">
+                <h5 class="modal-title" id="phone-details">${data.strMeal}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-    </div>
+            <div class="modal-body">
+                <img src='${data.strMealThumb}' class='img-fluid' />
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
     
     `;
+    modalContent.innerHTML = '';
+    modalContent.appendChild(div);
 
-    allItem.appendChild(div);
 }
