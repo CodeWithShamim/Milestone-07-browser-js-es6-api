04141c8adfb58be822c368654f79f17a2eb015e8
codewithshamim <shamimislamonline@gmail.com>
2022-03-08 11:04:15 +0600

    js fundamentals concept

---
+++ Module 42 (JS recap & react preparation)/fundamentals.js
@@ -0,0 +1,10 @@
+// 1. how to declare variable using let const 
+// 2. condition
+// 3.array
+const myArray = [10, 20, 30, 40, 50, 60];
+console.log(myArray.length);
+console.log(myArray.indexOf(20));
+console.log(myArray.includes(20));
+// 4.loop
+// 5.function
+// 6.object
