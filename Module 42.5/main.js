36b84044412432b1c18b8c96a8b081f53877f0d5
codewithshamim <shamimislamonline@gmail.com>
2022-03-09 17:54:44 +0600

    close button error handle added

--- Module 42.5/main.js
+++ Module 42.5/main.js
@@ -23,7 +23,8 @@ function submitIssue(e) {
 
 const closeIssue = id => {
     const issues = JSON.parse(localStorage.getItem('issues'));
-    const currentIssue = issues.find(issue => issue.id === id);
+    const currentIssue = issues.find(issue => issue.id === (id + ''));
+    // console.log(currentIssue.status);
     currentIssue.status = 'Closed';
     localStorage.setItem('issues', JSON.stringify(issues));
     fetchIssues();
@@ -49,8 +50,10 @@ const fetchIssues = () => {
                               <h3> ${description} </h3>
                               <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
                               <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
-                              <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
+                              <a href="#" onclick="closeIssue(${id})" class="btn btn-warning">Close</a>
                               <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
                               </div>`;
     }
 }
+
+// setStatusClosed
