49bb7429e4fee8bb9eba18062b1c79f7eb2016c7
codewithshamim <shamimislamonline@gmail.com>
2022-03-13 17:06:09 +0600

    done

--- Module 43.5/js/app.js
+++ Module 43.5/js/app.js
@@ -1,6 +1,8 @@
 const copy = () => {
     // alert('hello');
     const text = document.createElement('p');
-    text.innerText = "Welcome to this esssssssssssnvironment";
+    text.innerText = "Welcome";
     navigator.clipboard.writeText(text.innerText);
+    console.log(text);
+    alert(`successfully copy this  "${text.innerText}"  text`)
 }
